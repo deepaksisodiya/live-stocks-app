36f5d361f08d00f13c455aee3e1caec78a781b82
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-03-04 19:57:16 +0530

    remove files, initial web socket changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    var socket = new WebSocket('ws://stocks.mnet.website');
+
+    socket.onerror = function(error) {
+      console.log('WebSocket Error: ' + error);
+    };
+  
+    socket.onopen = function(event) {
+      console.log('socket is open')
+    };
+
+    socket.onmessage = function(event) {
+      console.log('on message ', event.data);
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          live stock app
         </p>
       </div>
     );
