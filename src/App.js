156fcc588febe043198fac84291e7986d0952ea5
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2018-03-04 20:52:15 +0530

    data manipulation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+
+import LiveStocks from "./Component/LiveStocks";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    error: false,
+    data: []
+  };
 
   componentDidMount() {
-    var socket = new WebSocket('ws://stocks.mnet.website');
+    var socket = new WebSocket("ws://stocks.mnet.website");
 
-    socket.onerror = function(error) {
-      console.log('WebSocket Error: ' + error);
-    };
-  
-    socket.onopen = function(event) {
-      console.log('socket is open')
+    socket.onerror = (error) => {
+      console.log("WebSocket Error: " + error);
     };
 
-    socket.onmessage = function(event) {
-      console.log('on message ', event.data);
+    socket.onopen = (event) => {
+      console.log("socket is open");
+    };
+
+    socket.onmessage = (event) => {
+      const data = [];
+      const message = JSON.parse(event.data);
+
+      message.forEach(([name, price]) => {
+        const obj = {
+          name,
+          price
+        };
+        data.push(obj);
+      });
+      
+      this.setState({
+        data,
+      });
     };
   }
 
-
   render() {
+    console.log('data ', this.state.data);
     return (
       <div className="App">
-        <p className="App-intro">
-          live stock app
-        </p>
+        <p className="App-intro">live stock app</p>
       </div>
     );
   }
